d104bc5fe36bb7086a79361a7a37a6275c80803a
Kayla Reopelle <kreopelle@gmail.com>
2018-01-16 20:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object, { [key]: value})
+  Object.assign({}, object, { [key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+  return object;
 }
